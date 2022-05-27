ffa8d7359dbbcc06c004e4f0283fa6ac46a0729d
LilaPavliuk <Liliia.Pavliuk1@gmail.com>
2022-05-27 14:33:28 +0000

    gallery css modification

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -3,7 +3,7 @@ const modal = document.querySelector(".modal");
 const modalimg = document.querySelector(".modalimg");
 const close = document.querySelector(".close");
 
-gallery_grids.forEach(image) => {
+gallery_grids.forEach(image => {
     image.addEventListener("click", () => {
         modalimg.src= image.src;
         modal.classList.add("appear");
