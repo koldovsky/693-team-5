92b728bae49cafe25dd1f46ee7b12d36c98d3080
LilaPavliuk <Liliia.Pavliuk1@gmail.com>
2022-05-27 14:57:33 +0000

    gallery js

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -1,11 +1,11 @@
-const gallery_grids = document.querySelectorAll("gallery_grids img");
+const gallery_grids = document.querySelectorAll(".gallery_grids img");
 const modal = document.querySelector(".modal");
 const modalimg = document.querySelector(".modalimg");
 const close = document.querySelector(".close");
 
 gallery_grids.forEach(image => {
     image.addEventListener("click", () => {
-        modalimg.src= image.src;
+        modalimg.src = image.src;
         modal.classList.add("appear");
         close.addEventListener("click", () => {
             modal.classList.remove("appear");
