484c82cc61db69c52107a42c9cf0058cd9cd22ec
LilaPavliuk <Liliia.Pavliuk1@gmail.com>
2022-05-27 16:40:37 +0000

    added height

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -1,16 +1,16 @@
-(function (){
-const gallery_grids = document.querySelectorAll(".gallery_grids img");
-const modal = document.querySelector(".modal");
-const modalimg = document.querySelector(".modal-img");
-const close = document.querySelector(".close");
+(function () {
+    const gallery_grids = document.querySelectorAll(".gallery_grids img");
+    const modal = document.querySelector(".modal");
+    const modalimg = document.querySelector(".modal-img");
+    const close = document.querySelector(".close");
 
-gallery_grids.forEach(image => {
-    image.addEventListener("click", () => {
-        modalimg.src = image.src;
-        modal.classList.add("appear");
-        close.addEventListener("click", () => {
-            modal.classList.remove("appear");
+    gallery_grids.forEach(image => {
+        image.addEventListener("click", () => {
+            modalimg.src = image.src;
+            modal.classList.add("appear");
+            close.addEventListener("click", () => {
+                modal.classList.remove("appear");
+            });
         });
     });
-});
 })();
