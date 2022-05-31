832eb643c5514eaeb48d3e1eae5df0784030a05e
LilaPavliuk <Liliia.Pavliuk1@gmail.com>
2022-05-31 12:43:53 +0000

    added slider js

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -1,16 +1,50 @@
-(function () {
-    const slides = document.querySelectorAll(".modal-content");
-    let index = 0;
-
-
-    function next() {
-        slides[index].classList.remove("active");
-        index = (index + 1) % slides.length;
-        slides[index].classList.add("active");
+// 
+(function (){
+const slides = [
+    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/Image1.png",
+    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image2.png",
+    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image3.png",
+    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image4.png",
+    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image5.png"
+  ];
+  
+  let currentSlide;
+  
+  function next() {
+    getCurrentSlide();
+    currentSlide++;
+    if (currentSlide === slides.length) {
+        currentSlide =0;
     }
-    function prev() {
-        slides[index].classList.remove("active");
-        index = (index - 1 + slides.length) % slides.length;
-        slides[index].classList.add("active");
+    imgUpdate();
+  }
+  
+  function prev() {
+    getCurrentSlide();
+    currentSlide--;
+    if (currentSlide === -1) {
+        currentSlide = slides.length - 1;
     }
-})()
+    imgUpdate();
+  }
+  
+  function getCurrentSlide() {
+    return currentSlide = slides.indexOf(document.querySelector(".modal-img").src);
+  }
+  
+  function imgUpdate() {
+    let img = document.querySelector(".modal-img");
+    img.src = slides[currentSlide];
+  }
+  
+  const buttonNext = document.querySelector(".next");
+  const buttonPrev = document.querySelector(".prev");
+  
+  buttonNext.addEventListener("click", () => {
+    next();
+  });
+  
+  buttonPrev.addEventListener("click", () => {
+    prev();
+  });
+})();
