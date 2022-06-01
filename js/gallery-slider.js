b3d20fa7e3b11adad354e1716120dbfde4133d89
LilaPavliuk <Liliia.Pavliuk1@gmail.com>
2022-06-01 09:55:02 +0000

    added src for gallery modal

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -2,7 +2,7 @@
 (function (){
 const slides = [
     "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/Image1.png",
-    "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image2.png",
+    "https://3000-koldovsky-693team5-9x7qibni8u6.ws-eu46.gitpod.io/img/img_gallery/image2.png",
     "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image3.png",
     "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image4.png",
     "https://3000-koldovsky-693team5-s59emz63t2j.ws-eu46.gitpod.io/img/img_gallery/image5.png"
@@ -29,11 +29,11 @@ const slides = [
   }
   
   function getCurrentSlide() {
-    return currentSlide = slides.indexOf(document.querySelector(".modal-img").src);
+    return currentSlide = slides.indexOf(document.querySelector(".modalimg").src);
   }
   
   function imgUpdate() {
-    let img = document.querySelector(".modal-img");
+    let img = document.querySelector(".modalimg");
     img.src = slides[currentSlide];
   }
   
