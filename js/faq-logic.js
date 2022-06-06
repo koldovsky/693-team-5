cb72df0016b842535c664d0ccd51a34a412ff819
Maksym <markovikmax@gmail.com>
2022-06-06 15:44:52 +0000

    FAQ section animation fix

--- js/faq-logic.js
+++ js/faq-logic.js
@@ -1,14 +1,9 @@
-var coll = document.getElementsByClassName("faq__collapsible");
-var i;
+const coll = document.getElementsByClassName("faq__collapsible");
+const cont = document.getElementsByClassName("faq__content");
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+    cont[i].classList.toggle("active");
   });
 }
